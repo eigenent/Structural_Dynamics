6e541c959b94046d3ba46507d48f78c4948a877f
eigenent <132782463+eigenent@users.noreply.github.com>
2023-05-20 14:03:36 +0300

    Update beam_stiffness.m
    Corrected the [8,2] cell in the 2 node matrix, it was Iy instead of Iz and caused K to not equal to Ktranspose. It is fixed now

--- beam_stiffness.m
+++ beam_stiffness.m
@@ -43,7 +43,7 @@ L = Length/nel; % element length, assuming equal spacing
 
 
 Kel= [E*A/L       0        0        0        0        0        -E*A/L        0        0        0        0        0;
-        0    12*E*Iz/L^3   0        0        0   6*E*Iz/L^2       0   -12*E*Iy/L^3    0        0        0   6*E*Iz/L^2;
+        0    12*E*Iz/L^3   0        0        0   6*E*Iz/L^2       0   -12*E*Iz/L^3    0        0        0   6*E*Iz/L^2;
         0         0  12*E*Iy/L^3    0  -6*E*Iy/L^2    0           0          0  -12*E*Iy/L^3   0   -6*E*Iy/L^2   0;
         0         0        0      G*J/L      0        0           0          0        0      -G*J/L     0        0;
         0         0  -6*E*Iy/L^2    0     4*E*Iy/L    0           0          0    6*E*Iy/L^2   0     2*E*Iy/L    0;
